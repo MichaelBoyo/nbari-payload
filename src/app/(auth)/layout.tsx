f68b8ea3f9f94b9ea686bf7d3e217b2ff8525e85
michaelboyo <103725264+MichaelBoyo@users.noreply.github.com>
2025-01-28 12:36:27 +0100

    auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,6 @@ import { cn } from '@/utilities/ui'
 import { Inter } from 'next/font/google'
 import React from 'react'
 
-import { Header } from '@/Header/Component'
 import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
@@ -27,11 +26,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
-      <body className="space-y-3">
-        <Providers>
-          <Header />
-          {children}
-        </Providers>
+      <body>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
