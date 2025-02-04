78c8f337e94219e5d46135d1481650265ab8aea2
michaelboyo <103725264+MichaelBoyo@users.noreply.github.com>
2025-02-04 20:19:10 +0100

    canges

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -47,9 +47,11 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         ...classNames,
       }}
       components={{
+        // @ts-ignore
         IconLeft: ({ className, ...props }) => (
           <ChevronLeft className={cn('h-4 w-4', className)} {...props} />
         ),
+        // @ts-ignore
         IconRight: ({ className, ...props }) => (
           <ChevronRight className={cn('h-4 w-4', className)} {...props} />
         ),
