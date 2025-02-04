ed7ef757f8724ed2aa6c447f6ac396af8e3b7cd9
michaelboyo <103725264+MichaelBoyo@users.noreply.github.com>
2025-02-04 21:28:21 +0100

    ij

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,4 @@
 // storage-adapter-import-placeholder
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 
 import sharp from 'sharp' // sharp-import
